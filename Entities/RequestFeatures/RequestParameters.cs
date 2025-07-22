cfc26dfbd50bdb04605903ab461248436c9f4567
uluagac <ersinuluagac@gmail.com>
2025-07-22 21:51:13 +0300

    OrderQuery

--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -5,12 +5,11 @@
         const int maxPageSize = 50;
         public int PageNumber { get; set; }
         private int _pageSize;
-
         public int PageSize
         {
             get { return _pageSize; }
             set { _pageSize = value > maxPageSize ? maxPageSize : value; }
         }
-
+        public string? OrderBy { get; set; }
     }
 }
