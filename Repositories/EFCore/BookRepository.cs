2d240a880cd99f3d4f6a2f8a57d2d58bd25a518e
uluagac <ersinuluagac@gmail.com>
2025-08-03 15:56:03 +0300

    API versioning

--- Repositories/EFCore/BookRepository.cs
+++ Repositories/EFCore/BookRepository.cs
@@ -14,7 +14,9 @@ namespace Repositories.EFCore
         }
 
         public void CreateOneBook(Book book) => Create(book);
+
         public void DeleteOneBook(Book book) => Delete(book);
+
         public async Task<PagedList<Book>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
         {
             var books = await FindAll(trackChanges)
@@ -26,9 +28,18 @@ namespace Repositories.EFCore
             return PagedList<Book>
                 .ToPagedList(books, bookParameters.PageNumber, bookParameters.PageSize);
         }
+
+        public async Task<List<Book>> GetAllBooksAsync(bool trackChanges)
+        {
+            return await FindAll(trackChanges)
+                .OrderBy(b => b.Id)
+                .ToListAsync();
+        }
+
         public async Task<Book> GetOneBookByIdAsync(int id, bool trackChanges) =>
             await FindByCondition(b => b.Id.Equals(id), trackChanges)
             .SingleOrDefaultAsync();
+
         public void UpdateOneBook(Book book) => Update(book);
     }
 }
